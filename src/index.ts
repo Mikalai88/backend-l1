3ed5f5a6514e863792416f667aae6c8709ff8edb
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-11-11 13:47:33 +0100

    id for adresses added

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const products = [{title: 'tomato'}, {title: "orange"}]
-const adresses = [{value: 'Nezalejnasti 12'}, {value: 'Selikaga 11'}]
+const adresses = [{id: "1", value: 'Nezalejnasti 12'}, {id: "2", value: 'Selikaga 11'}]
 
 app.get('/products', (req: Request, res: Response) => {
     res.send(products)
@@ -18,11 +18,18 @@ app.get('/products/:productTitle', (req: Request, res: Response) => {
     } else {
         res.send(404)
     }
-
-
 })
 app.get('/adresses', (req: Request, res: Response) => {
     res.send(adresses)
+})
+app.get('/adresses/:id', (req: Request, res: Response) => {
+    let adresse = adresses.find(a => a.id === req.params.id)
+
+    if (adresse) {
+        res.send(adresse)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(port, () => {
