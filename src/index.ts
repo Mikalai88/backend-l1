048ea038beb619b9aea14c7b32574d1352595105
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-11-11 14:44:49 +0100

    bodyParser added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 const products = [{id: "1", title: 'tomato'}, {id: "2", title: "orange"}]
 const adresses = [{id: "1", value: 'Nezalejnasti 12'}, {id: "2", value: 'Selikaga 11'}]
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
 
 app.get('/products', (req: Request, res: Response) => {
     if (req.query.title) {
@@ -14,8 +18,15 @@ app.get('/products', (req: Request, res: Response) => {
         res.send(products)
     }
 })
+app.post('/products', (req: Request, res: Response) => {
+    const newProduct = {
+        id: (new Date()).toString(),
+        title: req.body.title
+    }
+    products.push(newProduct)
+    res.status(201).send(newProduct)
+})
 app.get('/products/:id', (req: Request, res: Response) => {
-    req.params.id
     let product = products.find(p => p.id === req.params.id)
 
     if (product) {
@@ -23,6 +34,17 @@ app.get('/products/:id', (req: Request, res: Response) => {
     } else {
         res.send(404)
     }
+})
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === req.params.id) {
+            products.splice(i, 1)
+            res.send(204)
+            return;
+        }
+        res.send(404)
+    }
+
 })
 app.get('/adresses', (req: Request, res: Response) => {
     res.send(adresses)
