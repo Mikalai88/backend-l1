c3cec983f969f8ce066f08d3e02f450747a14133
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-11-12 12:21:04 +0100

    dz fix15

--- src/setting.ts
+++ src/setting.ts
@@ -58,9 +58,9 @@ app.post('/videos', (req: Request, res: Response) => {
     ) !== "string") {
         errorsMessages.push({"message":"author is required","field":"author"})
     }
-    if (!availableResolutions || availableResolutions !== "P144" || availableResolutions !== "P240" ||
-        availableResolutions !== "360" || availableResolutions !== "480" || availableResolutions !== "720" ||
-        availableResolutions !== "1440" || availableResolutions !== "2160") {
+    if (!availableResolutions || !availableResolutions.includes("P144") || !availableResolutions.includes("P240") ||
+        !availableResolutions.includes("P360") || !availableResolutions.includes("P480") || !availableResolutions.includes("720") ||
+        !availableResolutions.includes("P1440") || !availableResolutions.includes("P2160")) {
         errorsMessages.push({"message":"availableResolutions is required","field":"availableResolutions"})
     }
 
